3c785b17effd6acd2b2f4e9c4be6bd0714a41345
Asad Chowdhury <asadchowdhury@Asads-Mac-Studio.local>
2026-02-24 03:54:49 +0600

    UI created for all tab

--- scripts.js/scripts.js
+++ scripts.js/scripts.js
@@ -1,1 +1,9 @@
-console.log("script file loaded");
+document.getElementById("all-tab").addEventListener("click", function (event) {
+  allTab = this.getElementsByClassName("btn-tab");
+  for (let tab of allTab) {
+    tab.classList.remove("bg-blue-400", "text-white");
+  }
+  let selectedTab = event.target;
+  selectedTab.classList.add("bg-blue-400", "text-white");
+});
+
